b190a8cecbffbfe44f872a65cff0cdb6a4c7eece
SashaTheGray <contact-sasha@proton.me>
2022-07-20 23:23:28 +0000

    feat(installer): Add package manager setup section

--- installer/install.sh
+++ installer/install.sh
@@ -1,8 +1,40 @@
+#!/bin/bash
 # This script installs Thesis.
 
-# 1. Install packages.
+# GLOBALS
+PACKAGE_MANAGER="NONE"
 
-# 2. Set zsh as shell if it isn't already.
+# DETERMINE SYSTEM'S PACKAGE MANAGER.
+
+# Declare a system -> package manager associative array.
+declare -A PM_LOOKUP=(
+    [/etc/debian_version]="apt"
+)
+
+# Determine package manager.
+for file in ${!PM_LOOKUP[@]};
+do
+    echo "[*] Thesis is determining system's package manager"
+    if [[ -f $file ]]; then
+        PACKAGE_MANAGER=${PM_LOOKUP[$file]}
+        echo "[+] Thesis has found $PACKAGE_MANAGER"
+        break
+    fi
+done
+
+# Abort if no suitable package manager was found.
+if [ $PACKAGE_MANAGER = "NONE" ]; then
+    echo "[-] Thesis cannot determine the systems package manager"
+    echo "[*] Thesis is aborting installation ..."
+    exit 1
+
+# INSTALL PACKAGES
+
+
+
+# STOW SYMLINKS
+
+# ZSH SETUP.
 if ["$SHELL" != "$(which zsh)"]; then
     
     # Get path of the zsh executable.
@@ -13,9 +45,6 @@ if ["$SHELL" != "$(which zsh)"]; then
     
     # Explictly set the current shell as zsh.
     export SHELL=$zsh_path
-
-    # Run zsh.
-    exec zsh
 fi
 
-# 3. Stow symlinks.
+
