88afcbfe1694910cf288c69700daabf8f5af3248
SashaTheGray <contact-sasha@proton.me>
2022-07-20 02:46:41 +0000

    feat(installer): Add zsh install sequence

--- installer/install.sh
+++ installer/install.sh
@@ -3,5 +3,10 @@
 # 1. Install packages.
 
 # 2. Set zsh as shell if it isn't already.
+if ["$SHELL" != "$(which zsh)"]; then
+    zsh_path=$(which zsh)
+    sudo chsh -s $zsh_path $USER
+    export SHELL=$zsh_path
+fi
 
 # 3. Stow symlinks.
