3dee0916806f8ad181e766e211bd08e9545ffdae
SashaTheGray <contact-sasha@proton.me>
2022-07-20 22:24:03 +0000

    feat(installer): Add steps to zsh setup

--- installer/install.sh
+++ installer/install.sh
@@ -4,9 +4,18 @@
 
 # 2. Set zsh as shell if it isn't already.
 if ["$SHELL" != "$(which zsh)"]; then
+    
+    # Get path of the zsh executable.
     zsh_path=$(which zsh)
+
+    # Change shell for $USER to zsh.
     sudo chsh -s $zsh_path $USER
+    
+    # Explictly set the current shell as zsh.
     export SHELL=$zsh_path
+
+    # Run zsh.
+    exec zsh
 fi
 
 # 3. Stow symlinks.
